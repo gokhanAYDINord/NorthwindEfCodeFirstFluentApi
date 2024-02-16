e74ad0e8bc91510fe2eaa3d3a78380b7e3aacd4e
gokhanAYDINord <119927371+gokhanAYDINord@users.noreply.github.com>
2024-02-16 15:47:51 +0300

    Docker dosyaları eklendi.

---
+++ arc_realtime/docker/mongo/init.js
@@ -0,0 +1,12 @@
+db.createUser(
+        {
+            user: "arcuser",
+            pwd: "3lm31K3l3m*!",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "ArcDSS"
+                }
+            ]
+        }
+);
